05fbc8091f78d72b37a02e94234bf42f76ce3d2b
iteathen <josh.oshiro@gmail.com>
2026-09-05 23:16:22 -0700

    Add LatticeKnight TensorProgram mapping seam

---
+++ tools/latticeknight-fp32-tensor-program.mjs
@@ -0,0 +1,7 @@
+export const LATTICEKNIGHT_FP32_PROGRAM_PROFILE = 'latticeknight-4m-fp32-tensor-program-v1';
+
+export function buildLatticeKnightFp32TensorProgram() {
+  const error = new Error('Frozen LatticeKnight FP32 TensorProgram mapping is not implemented.');
+  error.code = 'VECTOR_LATTICEKNIGHT_TENSOR_PROGRAM_UNIMPLEMENTED';
+  throw error;
+}
